0be0a193f3993ffc16e9d805070a8bc9f402e4cb
josegallo <josegallo@gmail.com>
2016-10-11 02:04:45 +0200

    Prepare the web for login
    - Update the bbdd
    - Change the .htaccess

--- bbdd_example/ci_db.sql
+++ bbdd_example/ci_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 01-10-2016 a las 13:46:57
+-- Tiempo de generación: 11-10-2016 a las 01:59:56
 -- Versión del servidor: 10.1.16-MariaDB
 -- Versión de PHP: 5.6.24
 
@@ -23,28 +23,84 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `ci_sessions`
+--
+
+CREATE TABLE `ci_sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(45) NOT NULL DEFAULT '0',
+  `user_agent` varchar(120) NOT NULL,
+  `last_activity` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `email` varchar(70) NOT NULL,
+  `order_time` date NOT NULL,
+  `shirt_size` varchar(10) DEFAULT NULL,
+  `stripe_choice` varchar(20) NOT NULL,
+  `free_shipping` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `orders`
+--
+
+INSERT INTO `orders` (`id`, `email`, `order_time`, `shirt_size`, `stripe_choice`, `free_shipping`) VALUES
+(46, 'josegallo@gmail.com', '2016-10-09', 'small', 'value 2', 1),
+(47, 'adelapereira@gmail.com', '2016-10-10', 'xlarge', 'value 3', 1),
+(48, 'adelapereira@gmail.com', '2016-10-10', 'xlarge', 'value 3', 1),
+(49, 'adelapereira@gmail.com', '2016-10-10', 'xlarge', 'value 3', 1),
+(50, 'josegallo@gmail.com', '2016-10-11', 'small', 'value 2', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `firstname` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL
+  `username` varchar(15) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `first_name` varchar(40) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `firstname`, `email`) VALUES
-(1, 'jose', 'jose@example.com'),
-(2, 'adela', 'adela@example.com'),
-(3, 'jimena', 'jimena@example.com'),
-(4, 'silvia', 'silvia4@example.com');
+INSERT INTO `users` (`id`, `username`, `password`, `first_name`, `last_name`, `email`) VALUES
+(1, '', '', 'jose', '', 'jose@example.com'),
+(2, '', '', 'adela', '', 'adela@example.com'),
+(3, '', '', 'jimena', '', 'jimena@example.com'),
+(4, '', '', 'silvia', '', 'silvia4@example.com');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `ci_sessions`
+--
+ALTER TABLE `ci_sessions`
+  ADD PRIMARY KEY (`session_id`),
+  ADD KEY `last_activity_idx` (`last_activity`);
+
+--
+-- Indices de la tabla `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `users`
@@ -56,6 +112,11 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
+--
+-- AUTO_INCREMENT de la tabla `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
